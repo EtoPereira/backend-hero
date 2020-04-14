4d17065169c7615d50a8e32f7469fd7274343466
Ciência da Computação IFRS <cienciadacompifrs@gmail.com>
2020-04-13 23:26:29 -0300

    alteracao

--- knexfile.js
+++ knexfile.js
@@ -69,7 +69,8 @@ module.exports = {
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: __dirname + '/database/migrations'
+      // tableName: 'knex_migrations'
     }
   }
 
